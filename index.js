b661189173d5af90096abc0faca50172ff3344cb
dvsadovskyi_LOCAL_WORK <dvsadovskyi>
2023-06-19 16:19:54 +0300

    keypress

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function App() {
     const [bank, setBank] = React.useState(true);
     const [volume, setVolume] = React.useState(50);
     const [display, setDisplay] = React.useState("");
+    const [active, setActive] = React.useState(false);
 
     React.useEffect(() => {
         function hide() {
@@ -14,6 +15,12 @@ function App() {
             clearTimeout(x)
         }
     }, [volume])
+
+    React.useEffect(() => {
+        document.addEventListener("keydown", handleKeyPress)
+        return () => { document.removeEventListener("keydown", handleKeyPress) }
+
+    }, [power, bank])
 
 
     function handleVolume(event) {
@@ -36,37 +43,80 @@ function App() {
 
     }
 
+    function handlePlay(event) {
+        if (power) {
+            const song = document.getElementById(event.target.textContent)
+            song.currentTime = 0;
+            song.play()
+            setDisplay(event.target.id)
+        }
+
+    }
+
+    function handleKeyPress(event) {
+        if (power) {
+            const songKey = event.key.toUpperCase()
+            const song = document.getElementById(songKey)
+            song.currentTime = 0;
+            song.play()
+            const refs1 = {
+                "Q": "Heater-1",
+                "W": "Heater-2",
+                "E": "Heater-3",
+                "A": "Heater-4",
+                "S": "Clap",
+                "D": "Open-HH",
+                "Z": "Kick-n'-Hat",
+                "X": "Kick",
+                "C": "Closed-HH",
+            }
+            const refs2 = {
+                "Q": "Heater-1",
+                "W": "Heater-1",
+                "E": "Heater-1",
+                "A": "Heater-1",
+                "S": "Heater-1",
+                "D": "Heater-1",
+                "Z": "Heater-1",
+                "X": "Heater-1",
+                "Q": "Heater-1",
+            }
+            setDisplay(bank ? refs1[songKey] : refs2[songKey])
+        }
+
+    }
+
 
     return (
         <div id="drum-machine">
             <div className="logo">LOGO</div>
             <div className="control-area">
                 <div className="pads">
-                    <div className="drum-pad" id={bank ? "Heater-1" : "Chord-1"}>Q
+                    <div onClick={handlePlay} className="drum-pad" id={bank ? "Heater-1" : "Chord-1"}>Q
                         <audio className="clip" src={bank ? "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" : "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"} id="Q"></audio>
                     </div>
-                    <div className="drum-pad" id={bank ? "Heater-2" : "Chord-2"}>W
+                    <div onClick={handlePlay} className="drum-pad" id={bank ? "Heater-2" : "Chord-2"}>W
                         <audio className="clip" src={bank ? "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" : "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"} id="W"></audio>
                     </div>
-                    <div className="drum-pad" id={bank ? "Heater-3" : "Chord-3"}>E
+                    <div onClick={handlePlay} className="drum-pad" id={bank ? "Heater-3" : "Chord-3"}>E
                         <audio className="clip" src={bank ? "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" : "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"} id="E"></audio>
                     </div>
-                    <div className="drum-pad" id={bank ? "Heater-4" : "Shaker"}>A
+                    <div onClick={handlePlay} className="drum-pad" id={bank ? "Heater-4" : "Shaker"}>A
                         <audio className="clip" src={bank ? "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" : "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"} id="A"></audio>
                     </div>
-                    <div className="drum-pad" id={bank ? "Clap" : "Open-HH"}>S
+                    <div onClick={handlePlay} className="drum-pad" id={bank ? "Clap" : "Open-HH"}>S
                         <audio className="clip" src={bank ? "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" : "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"} id="S"></audio>
                     </div>
-                    <div className="drum-pad" id={bank ? "Open-HH" : "Closed-HH"}>D
+                    <div onClick={handlePlay} className="drum-pad" id={bank ? "Open-HH" : "Closed-HH"}>D
                         <audio className="clip" src={bank ? "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" : "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"} id="D"></audio>
                     </div>
-                    <div className="drum-pad" id={bank ? "Kick-n'-Hat" : "Punchy-Kick"}>Z
+                    <div onClick={handlePlay} className="drum-pad" id={bank ? "Kick-n'-Hat" : "Punchy-Kick"}>Z
                         <audio className="clip" src={bank ? "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" : "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"} id="Z"></audio>
                     </div>
-                    <div className="drum-pad" id={bank ? "Kick" : "Side-Stick"}>X
+                    <div onClick={handlePlay} className="drum-pad" id={bank ? "Kick" : "Side-Stick"}>X
                         <audio className="clip" src={bank ? "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" : "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"} id="X"></audio>
                     </div>
-                    <div className="drum-pad" id={bank ? "Closed-HH" : "Snare"}>C
+                    <div onClick={handlePlay} className="drum-pad" id={bank ? "Closed-HH" : "Snare"}>C
                         <audio className="clip" src={bank ? "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" : "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"} id="C"></audio>
                     </div>
                 </div>
